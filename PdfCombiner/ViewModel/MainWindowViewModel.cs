ff7d9e54ac49e0676037ac873b7337fbc15de226
n1developer-ubt <u.b.t.usama.bin.tariq@gmail.com>
2023-04-07 23:39:23 +0500

    Updated

--- PdfCombiner/ViewModel/MainWindowViewModel.cs
+++ PdfCombiner/ViewModel/MainWindowViewModel.cs
@@ -9,13 +9,12 @@ using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Xml.Linq;
 using Ghostscript.NET;
 using Ghostscript.NET.Rasterizer;
 using ImageMagick;
 using iTextSharp.text;
-using iTextSharp.text.pdf;
-using Microsoft.Win32;
-using PostSharp.Patterns.Diagnostics;
+using iTextSharp.text.pdf; 
 using PostSharp.Patterns.Model;
 using Syncfusion.Windows.PdfViewer;
 using ZFile;
@@ -29,33 +28,14 @@ namespace PdfCombiner.ViewModel
     [NotifyPropertyChanged]
     public class MainWindowViewModel
     {
-        private bool isTest = false;
-        public int LeftMargin { get; set; }
-        public int TopMargin { get; set; }
-        public int Spacing { get; set; } = 10;
+        private List<string> _files = new List<string>();
         public MainWindowViewModel()
-        {
-            FilesToMix = 18;
-            //DirectoryInfo d = new DirectoryInfo(@"C:\Users\Admin\Desktop\pddd\IMAGE");
-            //d.GetFiles("*.jpg").ToList().ForEach(f => SelectedFiles.Add(f.FullName));
-            //isTest = true;
-
+        { 
             InitCommand();
         }
 
         public async Task StartExport(string output)
-        {
-            var spacing = Spacing;
-            var width = 18 * 72;
-            var height = 12 * 72;
-
-            var iw = width / 6.0 - spacing;
-            var ih = height / 3.0 - spacing;
-
-            var margin = 0.45f;
-
-            float ySpacing = spacing / 2.0f - 0.2f, xSpacing = spacing / 2.0f - margin;
-
+        {  
             await Task.Run(() =>
             {
                 MainWindow.Main.Dispatcher.Invoke(() =>
@@ -64,55 +44,12 @@ namespace PdfCombiner.ViewModel
                     ProgressBarVisibility = Visibility.Visible;
                     Progress = 0;
                 });
-
-                var pgSize = new Rectangle(width, height);
-                var doc = new Document(pgSize, 0, 0, 0, 0);
-
+                  
                 if (File.Exists(output))
                     File.Delete(output);
 
-                PdfWriter.GetInstance(doc, new FileStream(output, FileMode.OpenOrCreate));
-
-                doc.Open();
-
-                int x = 0, y = 1, count = 0;
-
-                SelectedFiles.ToList().ForEach(f =>
-                {
-                    var fileName = isTest ? f : Path.Combine(Path.GetTempPath(), Path.GetTempFileName() + ".png");
-
-                    if (!isTest)
-                        ExportFile(f, fileName);
-
-
-                    var di = Image.GetInstance(fileName);
-                    di.ScaleAbsolute((float)ih, (float)iw);
-
-                    var xx = x * iw + xSpacing + LeftMargin;
-
-                    di.SetAbsolutePosition((float)xx, (float)(doc.Top - (ih * y) - ySpacing - TopMargin));
-
-                    di.Rotation = RotateLeft ? 1.5708f : -1.5708f;
-
-                    count++;
-
-                    x++;
-                    xSpacing += spacing;
-                    if (count % 6 == 0)
-                    {
-                        x = 0;
-                        xSpacing = spacing / 2.0f - margin;
-                        y++;
-                        ySpacing += spacing;
-                    }
-
-                    doc.Add(di);
-
-                    Progress++;
-                });
-
-                doc.Close();
-
+                CombineMultiplePdFs(_files.ToArray(), output);
+                
                 OutputFile = null;
                 OutputFile = output;
                 AllowAll = true;
@@ -120,35 +57,11 @@ namespace PdfCombiner.ViewModel
                 MessageBox.Show("File Exported!", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
             });
         }
-
-        private void ExportFile(string pdf, string output)
-        {
-            var settings = new MagickReadSettings();
-            // Settings the density to 300 dpi will create an image with a better quality
-            settings.Density = new Density(500, 500);
-
-            using (var images = new MagickImageCollection())
-            {
-                // Add all the pages of the pdf file to the collection
-                images.Read(pdf, settings);
-
-                var page = 1;
-                foreach (var image in images)
-                {
-                    // Write page to file that contains the page number
-                    image.Write(output);
-                    page++;
-                    break;
-                }
-            }
-        }
+        
 
         #region Props
 
-        public bool RotateLeft { get; set; } = true;
-        public string FolderName { get; set; }
-        public int FilesToMix { get; set; } = 5;
-        public ObservableCollection<string> SelectedFiles { get; set; } = new ObservableCollection<string>();
+        public string SelectedFolder { get; set; }
         public string OutputFile { get; set; }
 
         public int Progress { get; set; } = 0;
@@ -160,80 +73,43 @@ namespace PdfCombiner.ViewModel
 
         #region Commands
 
-        public ICommand SelectFileCommand { get; set; }
+        public ICommand SelectDirectoryCommand { get; set; }
         public ICommand ExportCommand { get; set; }
 
         private void InitCommand()
         {
-            SelectFileCommand = new DelegateCommand(o =>
+            SelectDirectoryCommand = new DelegateCommand(o =>
             {
-                var fs = new System.Windows.Forms.OpenFileDialog
-                {
-                    //Filter = "PDF Files (*.pdf)|*.pdf;",
-                    Multiselect = true
-                };
+                var fs = new FolderBrowserDialog();
 
                 if (fs.ShowDialog() == DialogResult.OK)
                 {
-                    SelectedFiles.Clear();
-                    fs.FileNames.ToList().ForEach(SelectedFiles.Add);
+                    SelectedFolder = fs.SelectedPath;
+                    var di = new DirectoryInfo(fs.SelectedPath);
+                    var files = di.GetFiles("*.pdf");
+                    foreach (var fileInfo in files.OrderBy(f=>f.Name))
+                    {
+                        _files.Add(fileInfo.FullName);
+                    }
                 }
             });
 
             ExportCommand = new DelegateCommand(async o =>
             {
-                if (SelectedFiles.Count != 18)
+                if (_files.Count == 0)
                 {
-                    MessageBox.Show("Please Select 18 Files!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show("No files found!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                     return;
                 }
 
-                if (isTest)
+                using (var sf = new SaveFileDialog())
                 {
-                    await StartExport("export.pdf");
-                }
-                else
-                {
-                    using (var sf = new SaveFileDialog())
+                    sf.Filter = "PDF Files (*.pdf)|*.pdf;";
+                    if (sf.ShowDialog() == DialogResult.OK)
                     {
-                        sf.Filter = "PDF Files (*.pdf)|*.pdf;";
-                        if (sf.ShowDialog() == DialogResult.OK)
-                        {
-                            await StartExport(sf.FileName);
-                        }
+                        await StartExport(sf.FileName);
                     }
                 }
-
-                return;
-                if (Directory.Exists(FolderName))
-                {
-                    using (var s = new SaveFileDialog())
-                    {
-                        s.Filter = "Pdf Document (*.pdf)|*.pdf;";
-
-                        if (s.ShowDialog() == DialogResult.OK)
-                        {
-                            ProgressBarVisibility = Visibility.Visible;
-                            var files = new DirectoryInfo(FolderName).GetFiles("*.pdf").ToList().OrderBy(a => new Guid());
-
-                            foreach (var fileInfo in files.Take(5))
-                            {
-                                SelectedFiles.Add(fileInfo.FullName);
-                            }
-
-                            await Task.Run(() =>
-                            {
-                                AllowAll = false;
-                                CombineMultiplePdFs(SelectedFiles.ToArray(), s.FileName);
-                                AllowAll = true;
-                                OutputFile = s.FileName;
-                                MessageBox.Show("File Exported", "Message", MessageBoxButton.OK,
-                                    MessageBoxImage.Information);
-                            });
-                        }
-                    }
-                }
-                else MessageBox.Show("Path not exist!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             });
         }
 
